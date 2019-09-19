7304a17b65cc086b42a46dc3d9738b4890413a04
williambl <william@williambl.com>
2019-09-19 10:12:11 +0100

    Add more useful information printing

--- src/main/kotlin/com/williambl/algorithmcomplexityfinder/AlgorithmComplexityFinder.kt
+++ src/main/kotlin/com/williambl/algorithmcomplexityfinder/AlgorithmComplexityFinder.kt
@@ -36,7 +36,8 @@ fun main() {
             |${pair.key}:
             |   Average for ${arrayLength/5}-length array: ${pair.value[arrayLength/5]?.average()} nanos
             |   Average for ${arrayLength}-length array: ${pair.value[arrayLength]?.average()} nanos
-            |   Difference: ${pair.value[arrayLength/5]?.average()?.minus(pair.value[arrayLength]?.average() ?: 0.0)} nanos
+            |   Difference: ${pair.value[arrayLength]?.average()?.minus(pair.value[arrayLength/5]?.average() ?: 0.0)} nanos
+            |   Factor: ${(pair.value[arrayLength]?.average()?.div(pair.value[arrayLength/5]?.average() ?: 0.0))} times
         """.trimMargin())
     }
 }
