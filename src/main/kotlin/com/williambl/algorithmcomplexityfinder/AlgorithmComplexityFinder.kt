4973954fa299b59fb5b7bb4cc94b697763eedab8
williambl <william@williambl.com>
2019-09-19 09:44:01 +0100

    Implement basic timing of algorithms

--- src/main/kotlin/com/williambl/algorithmcomplexityfinder/AlgorithmComplexityFinder.kt
+++ src/main/kotlin/com/williambl/algorithmcomplexityfinder/AlgorithmComplexityFinder.kt
@@ -1,2 +1,31 @@
 package com.williambl.algorithmcomplexityfinder
 
+import kotlin.random.Random
+import kotlin.system.measureNanoTime
+
+val attempts: Int = 5
+val arrayLength: Int = 50
+
+val algorithms: Map<String, (IntArray) -> Unit> = mapOf(
+    Pair("test", { _: IntArray -> println("test") })
+)
+
+fun main() {
+
+    val resultsAndAlgorithms: Map<String, MutableList<Long>> = algorithms.map { it.key to mutableListOf<Long>() }.toMap()
+    for (algorithm in algorithms) {
+        for (i in 0 until attempts) {
+            val input: IntArray = IntArray(arrayLength) { Random.nextInt() }
+            resultsAndAlgorithms[algorithm.key]?.add(
+                measureNanoTime {
+                    algorithm.value(input)
+                }
+            )
+        }
+    }
+
+
+    for (pair in resultsAndAlgorithms) {
+        println("${pair.key}: ${pair.value.average()} nanos")
+    }
+}
